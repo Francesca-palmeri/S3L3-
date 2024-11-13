b894ad42e2a0e8e25a562da30d903c7b55d5e340
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2024-11-13 18:06:12 +0100

    firt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
-const btnprintAdd = document.getElementById('printAdd');
-const inputBox = document.getElementById('typeBox');
-let list = document.getElementById('myList');
+const btnprintAdd = document.getElementById('printAdd'); // creata la variabile per selezionare l'elemento button in html
+const inputBox = document.getElementById('typeBox'); //creata la variabile per selezionare l'elemento di input in html
+const list = document.getElementById('myList'); //creata la variabile per selezionare l'elemento ul in html 
 
 
-btnprintAdd.addEventListener('click', function newElements());
+btnprintAdd.addEventListener('click', newElements); //Il gestore di eventi che esegue la funzione al click del button
 
-function newElements(){
-    if( )
+function newElements(){ 
+    if( inputBox.value === '' ){ //la condizione per la quale se l'inputBox non riceve nessun input  
+        alert('Scrivi qualcosa!')//eseguirà l'allert
+    } else {  //altrimenti
+        let li = document.createElement('li'); //creata la variabile per contenere gli elementi li 
+        li.innerHTML = inputBox.value; //viene stampato il valore(value) di inputBox all'interno del li appena creato
+        list.appendChild(li); //appeso il li appena creato con il valore stampato all'interno di ul
+        inputBox.value = ''; //svuota l'input
+      }
 }
-    
