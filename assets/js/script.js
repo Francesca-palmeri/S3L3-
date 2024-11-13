e8b4a54cc843443a8f11def3aa35f9c2855bd0e2
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2024-11-13 16:55:10 +0100

    first

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const btnprintAdd = document.getElementById('printAdd');
+const inputBox = document.getElementById('typeBox');
+let list = document.getElementById('myList');
+
+
+btnprintAdd.addEventListener('click', function newElements());
+
+function newElements(){
+    if( )
+}
+    
